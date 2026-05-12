6baa4be31d0bf6dfea9ac2bf946e595230175a9c
Giuseppe <TUA_EMAIL_GITHUB>
2026-05-12 23:09:05 +0200

    fix stripe import

--- backend/src/stripe/stripe.service.ts
+++ backend/src/stripe/stripe.service.ts
@@ -1,49 +1,43 @@
 import { Injectable } from '@nestjs/common';
+
 import Stripe from 'stripe';
 
 @Injectable()
 export class StripeService {
-  private stripe: any;
+  private stripe: Stripe;
 
   constructor() {
     this.stripe = new Stripe(
-      process.env.STRIPE_SECRET_KEY as string,
+      process.env.STRIPE_SECRET_KEY || '',
       {
-        apiVersion: '2026-04-22.dahlia',
+        apiVersion: '2025-04-30.basil',
       },
     );
   }
 
-  async createCheckoutSession() {
-    const session =
-      await this.stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-
-        line_items: [
-          {
-            price_data: {
-              currency: 'eur',
-
-              product_data: {
-                name: 'Biglietto Evento',
-              },
-
-              unit_amount: 1999,
+  async createCheckoutSession(
+    eventTitle: string,
+    price: number,
+  ) {
+    return this.stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price_data: {
+            currency: 'eur',
+            product_data: {
+              name: eventTitle,
             },
-
-            quantity: 1,
+            unit_amount: price * 100,
           },
-        ],
-
-        mode: 'payment',
-
-        success_url:
-          'http://localhost:3000?success=true',
-
-        cancel_url:
-          'http://localhost:3000?canceled=true',
-      });
-
-    return session.url;
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url:
+        'http://localhost:3000/success',
+      cancel_url:
+        'http://localhost:3000/cancel',
+    });
   }
 }
