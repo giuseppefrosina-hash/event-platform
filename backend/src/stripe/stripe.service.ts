18b31b68b5f88c76724f0ec59d5028db40fc2aae
Giuseppe <TUA_EMAIL_GITHUB>
2026-05-18 12:58:32 +0200

    fix stripe commonjs

--- backend/src/stripe/stripe.service.ts
+++ backend/src/stripe/stripe.service.ts
@@ -1,44 +1,48 @@
 import { Injectable } from '@nestjs/common';
-import Stripe from 'stripe';
+const Stripe = require('stripe');
 
 @Injectable()
 export class StripeService {
-  private stripe;
+  private stripe: any;
 
   constructor() {
-    this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-      apiVersion: '2026-04-22.dahlia',
-    });
+    this.stripe = new Stripe(
+      process.env.STRIPE_SECRET_KEY || '',
+      {
+        apiVersion: '2026-04-22.dahlia',
+      },
+    );
   }
 
   async createCheckoutSession(
     eventTitle: string,
     amount: number,
   ) {
-    const session = await this.stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
+    const session =
+      await this.stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
 
-      line_items: [
-        {
-          price_data: {
-            currency: 'eur',
+        line_items: [
+          {
+            price_data: {
+              currency: 'eur',
 
-            product_data: {
-              name: eventTitle,
+              product_data: {
+                name: eventTitle,
+              },
+
+              unit_amount: amount * 100,
             },
 
-            unit_amount: amount * 100,
+            quantity: 1,
           },
+        ],
 
-          quantity: 1,
-        },
-      ],
+        mode: 'payment',
 
-      mode: 'payment',
-
-      success_url: 'https://google.com',
-      cancel_url: 'https://google.com',
-    });
+        success_url: 'https://google.com',
+        cancel_url: 'https://google.com',
+      });
 
     return session.url;
   }
