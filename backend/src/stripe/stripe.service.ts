046161b9931a1ac000e2223a03bb2e710aebe11e
Giuseppe <TUA_EMAIL_GITHUB>
2026-05-18 12:46:59 +0200

    fix stripe constructor

--- backend/src/stripe/stripe.service.ts
+++ backend/src/stripe/stripe.service.ts
@@ -3,33 +3,43 @@ import Stripe from 'stripe';
 
 @Injectable()
 export class StripeService {
-  private stripe: any;
+  private stripe;
 
   constructor() {
-    this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '');
+    this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
+      apiVersion: '2025-04-30.basil',
+    });
   }
 
   async createCheckoutSession(
     eventTitle: string,
     amount: number,
   ) {
-    return await this.stripe.checkout.sessions.create({
+    const session = await this.stripe.checkout.sessions.create({
       payment_method_types: ['card'],
+
       line_items: [
         {
           price_data: {
             currency: 'eur',
+
             product_data: {
               name: eventTitle,
             },
+
             unit_amount: amount * 100,
           },
+
           quantity: 1,
         },
       ],
+
       mode: 'payment',
-      success_url: 'https://example.com/success',
-      cancel_url: 'https://example.com/cancel',
+
+      success_url: 'https://google.com',
+      cancel_url: 'https://google.com',
     });
+
+    return session.url;
   }
 }
