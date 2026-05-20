a26e4c85007e4fd471d240a29adfb13ebe86d9b2
Giuseppe <TUA_EMAIL_GITHUB>
2026-05-20 19:05:28 +0200

    remove turbopack warnings

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,18 +1,3 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
-
-  turbopack: {
-    root: __dirname,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
