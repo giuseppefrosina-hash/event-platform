daa7484d40f309fd673a5705e4a42f155345f536
Giuseppe <TUA_EMAIL_GITHUB>
2026-05-21 17:50:34 +0200

    register tickets module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,26 +1,38 @@
 import { Module } from '@nestjs/common';
 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { PrismaService } from './prisma/prisma.service';
+
 import { AuthModule } from './auth/auth.module';
+
 import { EventsModule } from './events/events.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { StripeModule } from './stripe/stripe.module';
-import { CloudinaryModule } from './upload/cloudinary/cloudinary.module';
+
 import { CompaniesModule } from './companies/companies.module';
+
 import { SuppliersModule } from './suppliers/suppliers.module';
+
 import { CollaboratorsModule } from './collaborators/collaborators.module';
+
 import { StaffModule } from './staff/staff.module';
+
+import { CloudinaryModule } from './upload/cloudinary/cloudinary.module';
+
 import { TicketsModule } from './tickets/tickets.module';
+
 @Module({
   imports: [
-    PrismaModule,
     AuthModule,
     EventsModule,
-    StripeModule,
-    CloudinaryModule,
     CompaniesModule,
     SuppliersModule,
     CollaboratorsModule,
     StaffModule,
+    CloudinaryModule,
+    TicketsModule,
   ],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
